43e3b0adb9d773f2d4b8d78d7207ce7a3e8491a3
Shourav111 <ri.shourav777@gmail.com>
2014-12-12 15:26:55 +0600

    new file added

---
+++ Problem2App/Problem2App/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem2App
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            double i, count, f1 = 0, f2 = 1, f3 = 0;
+            Console.Write("Enter the Limit : ");
+            count = double.Parse(Console.ReadLine());
+            Console.WriteLine(f1);
+            Console.WriteLine(f2);
+            for (i = 0; i <= count; i++)
+            {
+                f3 = f1 + f2;
+                f1 = f2;
+                f2 = f3;
+                    if ((f3 % 2) == 0 && (f3<=40000000))
+                   
+                    {
+                        Console.WriteLine(f3);   
+                }
+                   
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}
+
---
+++ largestprimefactorApp/largestprimefactorApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace largestprimefactorApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long dividend = 600851475143;
+            long n = 2;
+
+            while (n < dividend)
+            {
+                if (dividend % n == 0)
+                {
+                    dividend = dividend / n;
+
+                }
+                n++;
+            }
+            Console.WriteLine(n);
+            Console.ReadKey();
+
+        }
+    }
+}
